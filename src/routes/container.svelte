39e8830c0cad61bf67363307e5a9b05b0eb6f600
chris <chrsep@protonmail.com>
2022-01-26 16:19:16 +0700

    first commit

---
+++ src/routes/container.svelte
@@ -0,0 +1,4 @@
+<div>
+  test
+  <slot name="test" class="mr-3 bg-black text-white" />
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Container from "./container.svelte"
+</script>
+
+<h1 class="h-4 w-3">Welcome to SvelteKit</h1>
+<Container>
+  <p
+    slot="test"
+    let:class={className}
+    class="h-full w-full p-2 backdrop-blur-0 {className}"
+  >
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</Container>
